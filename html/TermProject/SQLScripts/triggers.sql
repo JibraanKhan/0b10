b2b1ad7cae3352d11e3e7a1cea0566cde786801c
Shelby <shelby.young@centre.edu>
2021-12-04 02:52:36 +0000

    maybe finish triggers?

--- html/TermProject/SQLScripts/triggers.sql
+++ html/TermProject/SQLScripts/triggers.sql
@@ -129,14 +129,14 @@ END IF;
 CREATE TRIGGER no_inactive_pokemon
 BEFORE INSERT ON Orders FOR EACH ROW
 IF is_pokemon_active(NEW.Inventory_ID) = FALSE THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Costume is inactive!';
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Pokemon has already been sold!';
 END IF;
 //
 
 CREATE TRIGGER no_inactive_pokemon_update
 BEFORE UPDATE ON Orders FOR EACH ROW
 IF is_pokemon_active(NEW.Inventory_ID) = FALSE THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Costume is inactive!';
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Pokemon has already been sold!';
 END IF;
 //
 
@@ -232,11 +232,19 @@ INSERT INTO Pokemon_Inventory(Pokemon_Name, Pokemon_Price,Pokemon_Active)
            ('Charmander',30,FALSE);
 
 INSERT INTO Customers (Cust_FirstName,Cust_LastName,Cust_Address,Cust_Phone)
-    VALUES ('Maran','Lee','100_Willow_Way','123-456-7890');
+    VALUES ('Maran','Lee','100_Willow_Way','123-456-7890'),
+           ('Will','Smith','urmomlane','222-333-4444');
 
 INSERT INTO Orders (Pokemon_Name,Cust_ID,Inventory_ID,Order_SoldFor)
     VALUES ('Bulbasaur',1,2,20);
+
+INSERT INTO Orders (Pokemon_Name,Cust_ID,Inventory_ID,Order_SoldFor)
+    VALUES ('Charmander',2,2,20);
+
            
+UPDATE Pokemon_Inventory 
+SET Pokemon_Price = -1
+WHERE Pokemon_Inventory.Inventory_ID = 1;          
 
 SELECT * FROM Sightings;
 SELECT * FROM Costumes_Rented;
